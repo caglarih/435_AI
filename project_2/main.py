39a02cbad88d95c3b153b40c6291c2616b3a1f6f
caglarih <caglarih@itu.edu.tr>
2016-12-01 11:46:56 +0300

    no error but doesnt stop

--- project_2/main.py
+++ project_2/main.py
@@ -1,5 +1,7 @@
 from simpleai.search import CspProblem
-
+from simpleai.search import backtrack
+from simpleai.search import MOST_CONSTRAINED_VARIABLE
+from simpleai.search import LEAST_CONSTRAINING_VALUE
 
 def get_variables():
     var = set()
@@ -34,6 +36,42 @@ def get_col(j):
     return set([str(10 * i + j) for i in range(1, 10)])
 
 
+def get_square(s):
+    res = set()
+    centers = [22, 25, 28, 52, 55, 58, 82, 82, 88]
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            res.add(str(centers[s - 1] + 10 * i + j))
+    return res
+
+
+def read_cons():
+    res = []
+    with open("cons.txt", "r") as f:
+        lines = f.read().split("\n")
+        for l in lines:
+            if not l == "":
+                res.append(l.split(" "))
+    return res
+
+
 if __name__ == "__main__":
+    constraints = []
     vs = get_variables()
     ds = get_domains(vs)
+
+    for i in range(1, 10):
+        constraints.append((get_row(i), const_different))
+        constraints.append((get_col(i), const_different))
+        constraints.append((get_square(i), const_different))
+
+    cs = read_cons()
+
+    for c in cs:
+        constraints.append((c[1:], sum_wrapper(int(c[0]))))
+
+    problem = CspProblem(vs, ds, constraints)
+    result = backtrack(problem,
+        variable_heuristic=MOST_CONSTRAINED_VARIABLE,
+        value_heuristic=LEAST_CONSTRAINING_VALUE)
+    print(result)
